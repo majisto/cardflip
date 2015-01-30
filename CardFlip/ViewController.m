3cbceadca7dce1a9c1073d46b9e34c7b216a43f6
Daniel Durbin <majisto@gmail.com>
2015-01-29 22:03:15 -0600

    Working on reset button.

--- CardFlip/ViewController.m
+++ CardFlip/ViewController.m
@@ -14,11 +14,11 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *shuffleLabel;
-@property (weak, nonatomic) IBOutlet UILabel *cardsDeckLabel;
+@property (weak, nonatomic) IBOutlet UILabel *games_playedLabel; //cardsDeckLabel
 @property (nonatomic) int flipCount;
-@property (nonatomic) NSUInteger cardsInDeck;
 @property (nonatomic) int numShuffles;
 @property (strong, nonatomic) Deck *myDeck;
+@property (nonatomic) NSUInteger games_played;
 
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttonArray;
 
@@ -32,9 +32,9 @@
         int i = 0;
         for (UIButton* b in self.buttonArray){
             [b setBackgroundImage:[UIImage imageNamed:@"white_image"] forState:UIControlStateNormal];
-//            Card * card = [self.game cardAtIndex:i];
-//            [b setTitle:card.contents forState:UIControlStateNormal];
-            [b setTitle:@(i).stringValue forState:UIControlStateNormal];
+            Card * card = [self.game cardAtIndex:i];
+            [b setTitle:card.contents forState:UIControlStateNormal];
+//            [b setTitle:@(i).stringValue forState:UIControlStateNormal];
             i++;
         }
         self.peeked = true;
@@ -50,6 +50,15 @@
 
 - (IBAction)resetClick:(UIButton *)sender {
     NSLog(@"Reset Button clicked!");
+    [self flipCardsOver];
+    self.games_played++;
+}
+
+-(void) flipCardsOver{
+    for (UIButton* b in self.buttonArray){
+        [b setBackgroundImage:[UIImage imageNamed:@"pokedoge"] forState:UIControlStateNormal];
+        [b setTitle:@"" forState:UIControlStateNormal];
+    }
 }
 
 -(Deck *) myDeck{
@@ -58,13 +67,13 @@
 }
 
 -(MatchingGame *) game{
-    if (!_game) {_game = [[MatchingGame alloc] init:29];}
+    if (!_game) {_game = [[MatchingGame alloc] init:30];}
     return _game;
 }
 
-- (void)setCardsInDeck:(NSUInteger)cardsInDeck{
-    _cardsInDeck = cardsInDeck;
-    self.cardsDeckLabel.text = [NSString stringWithFormat:@"Cards in deck: %lu", (unsigned long)self.cardsInDeck];
+- (void)setGames_played:(NSUInteger)games_played{
+    _games_played = games_played;
+    self.games_playedLabel.text = [NSString stringWithFormat:@"Games Played: %lu", (unsigned long)self.games_played];
 }
 
 - (void)setFlipCount:(int)flipCount{
@@ -100,13 +109,11 @@
         if (!randCard) { //If no cards in deck, re-shuffle by re-creating the myDeck object.
             _myDeck = [[PlayingCardDeck alloc] init];
             self.flipCount = 0;
-            self.cardsInDeck = 52;
             randCard = [self.myDeck drawRandCard];
             self.numShuffles++;
         }
         [sender setBackgroundImage:[UIImage imageNamed:@"white_image"] forState:UIControlStateNormal];
         [sender setTitle:randCard.contents forState:UIControlStateNormal];
-        self.cardsInDeck = _myDeck.numCards;
         self.flipCount++;
     }
     self.face_up_down = !self.face_up_down;
